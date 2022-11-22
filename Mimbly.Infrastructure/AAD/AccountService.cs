0889bca3f158acbc2b9a130a9a873a45fe5c6218
Hargaaya <hargaaya10@gmail.com>
2022-11-22 17:14:04 +0100

    add user invite method

--- Mimbly.Infrastructure/AAD/AccountService.cs
+++ Mimbly.Infrastructure/AAD/AccountService.cs
@@ -20,21 +20,40 @@ public class AccountService
     }
 
     // TODO: Update redirect url
-    public async void InviteUser(string email, string companyId, string? displayName)
+    public async void InviteUser(UserInviteModel user)
     {
         var client = _graphService.GetClient();
 
         var invitation = new Invitation
         {
-            InvitedUserDisplayName = displayName,
-            InvitedUserEmailAddress = email,
-            InviteRedirectUrl = $"{_redirectUrl}/dashboard/{companyId}"
+            InvitedUserDisplayName = user.DisplayName,
+            InvitedUserEmailAddress = user.EmailAddress,
+            InviteRedirectUrl = $"{_redirectUrl}/dashboard/{user.GroupId}",
         };
 
+        var userInfo = new User
+        {
+            JobTitle = user.Contact?.JobTitle,
+            MobilePhone = user.Contact?.MobilePhone,
+            StreetAddress = user.Contact?.StreetAddress,
+            City = user.Contact?.StreetAddress,
+            Country = user.Contact?.Country
+        };
+
+        try
+        {
         var invite = await client.Invitations.Request().AddAsync(invitation);
 
-        var newUserId = invite.InvitedUser.Id;
+            var invitedUserId = invite.InvitedUser.Id;
+
+            await client.Users[invitedUserId].Request().UpdateAsync(userInfo);
+            await client.Groups[user.GroupId].Members.References.Request().AddAsync(new DirectoryObject { Id = invitedUserId });
     }
+        catch (Exception ex)
+        {
+            // TODO: create a LoggerMessage Extension 
+            _logger.LogInformation("Something went wrong inviting a user.", ex);
+        }
 
     public async void InviteTechnician(string displayName, string email, string phoneNumber, string companyName, string Location)
     {
