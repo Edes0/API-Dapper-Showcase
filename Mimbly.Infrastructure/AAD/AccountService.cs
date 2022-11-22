b05f0eb23698f3f4ce862e14c31c584d66d1b9a7
Hargaaya <hargaaya10@gmail.com>
2022-11-22 21:21:30 +0100

    EoD: refactor service

--- Mimbly.Infrastructure/AAD/AccountService.cs
+++ Mimbly.Infrastructure/AAD/AccountService.cs
@@ -19,82 +19,46 @@ public class AccountService
         _logger = logger;
     }
 
-    // TODO: Update redirect url
+    // TODO: Add nullchecks, return values if successfull.
+
     public async void InviteUser(UserInviteModel user)
     {
-        var client = _graphService.GetClient();
         var redirectUrl = _redirectUrl.Path = "dashboard/" + user.GroupId;
 
         var userInvitation = GetInvitation(user, redirectUrl);
         var userInfo = GetUserInfo(user);
 
-        try
-        {
-            var invitedUserId = await InviteAndGetUserId(userInvitation);
-
-            var invitedUserId = resp.InvitedUser.Id;
-
-            await client.Users[invitedUserId].Request().UpdateAsync(userInfo);
-            await client.Groups[user.GroupId].Members.References.Request().AddAsync(new DirectoryObject { Id = invitedUserId });
-        }
-        catch (Exception ex)
-        {
-            // TODO: create a LoggerMessage Extension 
-            _logger.LogInformation("Something went wrong inviting a user.", ex);
-        }
+        var invitedUserId = await InviteAndGetUserId(userInvitation);
+        UpdateUserInfo(userInfo, invitedUserId);
+        AddMemberToGroup(user.GroupId, invitedUserId);
     }
 
     public async void InviteTechnician(UserInviteModel technician)
     {
-        var client = _graphService.GetClient();
-
         var userInvitation = GetInvitation(technician, _redirectUrl.ToString());
         var userInfo = GetUserInfo(technician);
 
-        try
-        {
-            var invitedUserId = await InviteAndGetUserId(userInvitation);
+        var invitedUserId = await InviteAndGetUserId(userInvitation);
+        UpdateUserInfo(userInfo, invitedUserId);
 
-            await client.Users[invitedUserId].Request().UpdateAsync(userInfo);
-
-            await client.Users[invitedUserId].Request().UpdateAsync(technicianInfo);
-
-            // TODO: Add technician to database, once entity is created
-        }
-        catch (Exception ex)
-        {
-            // TODO: create a LoggerMessage Extension 
-            _logger.LogInformation("Something went wrong inviting a technician.", ex);
-        }
+        // TODO: Add technician to database, once entity is created
     }
 
     public async void InviteAdmin(UserInviteModel admin)
     {
-        var client = _graphService.GetClient();
-
         var userInvitation = GetInvitation(admin, _redirectUrl.ToString());
         var userInfo = GetUserInfo(admin);
 
-        try
-        {
-            var invitedUserId = await InviteAndGetUserId(userInvitation);
+        var invitedUserId = await InviteAndGetUserId(userInvitation);
+        UpdateUserInfo(userInfo, invitedUserId);
 
-            var invitedUserId = resp.InvitedUser.Id;
-
-            // TODO: Insert admin group id, best case have in memory dicitionary of companyName and Ids.
-            await client.Users[invitedUserId].Request().UpdateAsync(userInfo);
-            await client.Groups["Insert admin id here"].Members.References.Request().AddAsync(new DirectoryObject { Id = invitedUserId });
-        }
-        catch (Exception ex)
-        {
-            // TODO: create a LoggerMessage Extension 
-            _logger.LogInformation("Something went wrong creating an admin", ex);
-        }
+        // TODO: Insert admin group id, best case have in memory dicitionary of companyName and Ids.
+        AddMemberToGroup("Admin group Id", invitedUserId);
     }
 
     public async void CreateCompany(UserInviteModel owner, string displayName, string description, Guid parentCompanyId)
     {
-        // TODO: Handle parentCompanyConnection, Create company instance in Db
+        // TODO: Create company instance in Db, Handle parentCompany relation 
         var client = _graphService.GetClient();
 
         var redirectUrl = _redirectUrl.Path = "dashboard/" + owner.GroupId;
@@ -102,56 +66,17 @@ public class AccountService
         var userInvitation = GetInvitation(owner, redirectUrl);
         var userInfo = GetUserInfo(owner);
 
-        var group = new Group
+        var groupInfo = new Group
         {
             DisplayName = displayName,
             Description = description,
         };
 
-        try
-        {
-            var resp = await client.Groups.Request().AddAsync(group);
-            var invite = await client.Invitations.Request().AddAsync(invitation);
+        var group = await client.Groups.Request().AddAsync(groupInfo);
+        var invitedUserId = await InviteAndGetUserId(userInvitation);
 
-            var groupId = resp.Id;
-            var invitedUserId = await InviteAndGetUserId(userInvitation);
-
-            await client.Users[invitedUserId].Request().UpdateAsync(userInfo);
-            await client.Groups[groupId].Owners.References.Request().AddAsync(new DirectoryObject { Id = invitedUserId });
-        }
-        catch (Exception ex)
-        {
-            // TODO: create a LoggerMessage Extension 
-            _logger.LogInformation("Something went wrong creating an admin", ex);
-        }
-    }
-
-    public async Task<bool> AssignRole(string email, string groupId)
-    {
-        var client = _graphService.GetClient();
-        try
-        {
-            var resp = await client.Users.Request().Filter($"mail eq '{email}'").GetAsync();
-            var user = resp.FirstOrDefault();
-            if (user != null)
-            {
-                return false;
-            }
-
-            var directoryObject = new DirectoryObject
-            {
-                Id = user.Id
-            };
-
-            await client.Groups[groupId].Members.References.Request().AddAsync(directoryObject);
-            return true;
-        }
-        catch (Exception ex)
-        {
-            // TODO: create a LoggerMessage Extension 
-            _logger.LogInformation("Something went wrong creating an admin", ex);
-            return false;
-        }
+        UpdateUserInfo(userInfo, invitedUserId);
+        AddOwnerToGroup(group.Id, invitedUserId);
     }
 
     public static Invitation GetInvitation(UserInviteModel user, string redirectUrl)
@@ -198,6 +123,51 @@ public class AccountService
             return null;
         }
     }
+
+    public async void UpdateUserInfo(User userInfo, string userId)
+    {
+        var client = _graphService.GetClient();
+
+        try
+        {
+            await client.Users[userId].Request().UpdateAsync(userInfo);
+        }
+        catch (Exception ex)
+        {
+            // TODO: create a LoggerMessage Extension 
+            _logger.LogInformation("Something went wrong inviting a user: ", ex);
+        }
     }
 
+    public async void AddMemberToGroup(string groupId, string userId)
+    {
+        var client = _graphService.GetClient();
+
+        try
+        {
+            var dirObj = new DirectoryObject { Id = userId };
+            await client.Groups[groupId].Members.References.Request().AddAsync(dirObj);
+        }
+        catch (Exception ex)
+        {
+            // TODO: create a LoggerMessage Extension 
+            _logger.LogInformation("Something went wrong adding a member to a group: ", ex);
+        }
+    }
+
+    public async void AddOwnerToGroup(string groupId, string userId)
+    {
+        var client = _graphService.GetClient();
+
+        try
+        {
+            var dirObj = new DirectoryObject { Id = userId };
+            await client.Groups[groupId].Owners.References.Request().AddAsync(new DirectoryObject { Id = userId });
+        }
+        catch (Exception ex)
+        {
+            // TODO: create a LoggerMessage Extension 
+            _logger.LogInformation("Something went wrong adding a member to a group: ", ex);
+        }
+    }
 }
