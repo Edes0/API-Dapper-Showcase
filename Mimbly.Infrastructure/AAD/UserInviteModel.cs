0b02afdc771b50b43b062a210c9d6576fde204b3
Hargaaya <hargaaya10@gmail.com>
2022-11-22 14:08:15 +0100

    add user invite model

---
+++ Mimbly.Infrastructure/AAD/UserInviteModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Mimbly.Infrastructure.AAD;
+
+using Microsoft.Graph;
+
+public class UserInviteModel
+{
+    public string? UserType { get; set; }
+    public bool IsGroupAdmin { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? DisplayName { get; set; }
+    public string? GroupId { get; set; }
+    public Contact? Contact { get; set; }
+}
+
+public class Contact
+{
+    public string? JobTitle { get; set; }
+    public string? MobilePhone { get; set; }
+    public string? StreetAddress { get; set; }
+    public string? City { get; set; }
+    public string? Country { get; set; }
+
+
+}
+
