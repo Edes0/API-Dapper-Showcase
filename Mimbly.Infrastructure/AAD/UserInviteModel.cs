0496af4b5165d01a61069d785199e38ba4ea332b
Hargaaya Idris <hargaaya10@gmail.com>
2022-11-23 15:08:53 +0100

    add data annotation and remove user type prop

--- Mimbly.Infrastructure/AAD/UserInviteModel.cs
+++ Mimbly.Infrastructure/AAD/UserInviteModel.cs
@@ -1,24 +1,33 @@
 ﻿namespace Mimbly.Infrastructure.AAD;
 
+using System.ComponentModel.DataAnnotations;
 using Microsoft.Graph;
 
 public class UserInviteModel
 {
-    public string? UserType { get; set; }
+    [Required]
+    [EmailAddress]
     public string? EmailAddress { get; set; }
+
+    [Required]
     public string? DisplayName { get; set; }
+
     public string? GroupId { get; set; }
+
     public Contact? Contact { get; set; }
 }
 
 public class Contact
 {
     public string? JobTitle { get; set; }
+
+    [Phone]
     public string? MobilePhone { get; set; }
+
     public string? StreetAddress { get; set; }
+
     public string? City { get; set; }
+
     public string? Country { get; set; }
-
-
 }
 
