ba2cfab5496c9c85be02e5b1bc1c1b1434399353
Hargaaya <hargaaya10@gmail.com>
2022-11-23 17:32:06 +0100

    add fluent validation to user invite model

--- Mimbly.Infrastructure/AAD/UserInviteModel.cs
+++ Mimbly.Infrastructure/AAD/UserInviteModel.cs
@@ -1,20 +1,24 @@
 ﻿namespace Mimbly.Infrastructure.AAD;
 
 using System.ComponentModel.DataAnnotations;
+using FluentValidation;
 using Microsoft.Graph;
+using Mimbly.CoreServices.Validation;
 
 public class UserInviteModel
 {
-    [Required]
-    [EmailAddress]
     public string? EmailAddress { get; set; }
 
-    [Required]
     public string? DisplayName { get; set; }
 
     public string? GroupId { get; set; }
 
     public Contact? Contact { get; set; }
+
+    public async Task Validate()
+    {
+        await ValidatableEntity.ValidateEntityByFluentRules(this, new UserInviteModelValidator());
+    }
 }
 
 public class Contact
@@ -29,5 +33,14 @@ public class Contact
     public string? City { get; set; }
 
     public string? Country { get; set; }
+}
+
+internal class UserInviteModelValidator : AbstractValidator<UserInviteModel>
+{
+    public UserInviteModelValidator()
+    {
+        RuleFor(user => user.EmailAddress).EmailAddress().NotNull();
+        RuleFor(user => user.DisplayName).NotNull();
+    }
 }
 
