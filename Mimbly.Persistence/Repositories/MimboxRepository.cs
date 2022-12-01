1e67062ab82cb4a7c5ee4478f13a7bf644d47bbe
Edes <sjogrenandreas@live.se>
2022-12-01 13:12:50 +0100

    Improve GetAllMimboxes with MimboxData

--- Mimbly.Persistence/Repositories/MimboxRepository.cs
+++ Mimbly.Persistence/Repositories/MimboxRepository.cs
@@ -22,15 +22,98 @@ public class MimboxRepository : IMimboxRepository
         DefaultTypeMap.MatchNamesWithUnderscores = true;
     }
 
-    public async Task<IEnumerable<Mimbox>> GetAllMimboxes()
+    public async Task CreateMimbox(Mimbox mimbox)
     {
         var sql =
         @"
-            SELECT *
-            FROM Mimbox
+            INSERT INTO Mimbox
+                (Id, Water_Saved, Co2_Saved, Plastic_Saved, Economy_Saved, Nickname, Mimbox_Status_Id, Mimbox_Model_Id, Mimbox_Location_Id, Company_Id)
+            VALUES
+                (@Id, @WaterSaved, @Co2Saved, @PlasticSaved, @EconomySaved, @Nickname, @StatusId, @ModelId, @LocationId, @CompanyId)
         ";
 
-        return await _db.LoadEntities<Mimbox, dynamic>(sql, new { });
+        await _db.SaveChanges(sql, mimbox);
+    }
+
+    public async Task DeleteMimbox(Mimbox mimbox)
+    {
+        var sql =
+        @"
+            DELETE
+            FROM Mimbox
+            WHERE Id = @Id
+        ";
+
+        await _db.SaveChanges(sql, mimbox);
+    }
+
+    public async Task UpdateMimbox(Mimbox mimbox)
+    {
+        var sql =
+        @"
+            UPDATE Mimbox
+            SET Water_Saved = @WaterSaved,
+                Co2_Saved = @Co2Saved,
+                Plastic_Saved = @PlasticSaved,
+                Economy_Saved = @EconomySaved,
+                Nickname = @Nickname,
+                Mimbox_Status_Id = @StatusId,
+                Mimbox_Model_Id = @ModelId,
+                Mimbox_Location_Id = @LocationId,
+                Company_Id = @CompanyId
+            WHERE Id = @Id
+        ";
+
+        await _db.SaveChanges(sql, mimbox);
+    }
+
+    public async Task<IEnumerable<Mimbox>> GetAllMimboxes()
+    {
+        var connectionString = _config.GetConnectionString(ConnectionStringName);
+        await using var connection = new SqlConnection(connectionString);
+
+        var sql =
+        @"
+            SELECT m.*, ml.*, ms.*, mm.*, mc.*, c.Id
+            FROM Mimbox m
+            LEFT JOIN Mimbox_Location ml ON ml.Id = m.Mimbox_Location_Id
+            LEFT JOIN Mimbox_Status ms ON ms.Id = m.Mimbox_Status_Id
+            LEFT JOIN Mimbox_Model mm ON mm.Id = m.Mimbox_Model_Id
+            LEFT JOIN Mimbox_Contact mc ON mc.Mimbox_Id = m.Id
+            LEFT JOIN Company c ON c.Id = m.Company_Id
+        ";
+
+        List<Mimbox> mimboxToReturn = new();
+
+        await connection.QueryAsync<Mimbox, MimboxLocation, MimboxStatus, MimboxModel, MimboxContact, Company, Mimbox>
+           (sql, (mimbox, mimboxLocation, mimboxStatus, mimboxModel, mimboxContact, company) =>
+           {
+               if (mimboxLocation != null)
+               {
+                   mimbox.Location = mimboxLocation;
+                   mimbox.LocationId = mimboxLocation.Id;
+               }
+
+               if (company != null)
+               {
+                   mimbox.CompanyId = company.Id;
+               }
+
+               mimbox.Model = mimboxModel;
+               mimbox.ModelId = mimboxModel.Id;
+               mimbox.Status = mimboxStatus;
+               mimbox.StatusId = mimboxStatus.Id;
+               mimbox.ContactList.Add(mimboxContact);
+
+               mimboxToReturn.Add(mimbox);
+
+               return null;
+           },
+           new
+           {
+           });
+
+        return mimboxToReturn;
     }
 
     public async Task<Mimbox> GetMimboxById(Guid id)
@@ -82,51 +165,6 @@ public class MimboxRepository : IMimboxRepository
            });
 
         return mimboxToReturn.FirstOrDefault();
-    }
-
-    public async Task CreateMimbox(Mimbox mimbox)
-    {
-        var sql =
-        @"
-            INSERT INTO Mimbox
-                (Id, Water_Saved, Co2_Saved, Plastic_Saved, Economy_Saved, Nickname, Mimbox_Status_Id, Mimbox_Model_Id, Mimbox_Location_Id, Company_Id)
-            VALUES
-                (@Id, @WaterSaved, @Co2Saved, @PlasticSaved, @EconomySaved, @Nickname, @StatusId, @ModelId, @LocationId, @CompanyId)
-        ";
-
-        await _db.SaveChanges(sql, mimbox);
-    }
-
-    public async Task DeleteMimbox(Mimbox mimbox)
-    {
-        var sql =
-        @"
-            DELETE
-            FROM Mimbox
-            WHERE Id = @Id
-        ";
-
-        await _db.SaveChanges(sql, mimbox);
-    }
-
-    public async Task UpdateMimbox(Mimbox mimbox)
-    {
-        var sql =
-        @"
-            UPDATE Mimbox
-            SET Water_Saved = @WaterSaved,
-                Co2_Saved = @Co2Saved,
-                Plastic_Saved = @PlasticSaved,
-                Economy_Saved = @EconomySaved,
-                Nickname = @Nickname,
-                Mimbox_Status_Id = @StatusId,
-                Mimbox_Model_Id = @ModelId,
-                Mimbox_Location_Id = @LocationId,
-                Company_Id = @CompanyId
-            WHERE Id = @Id
-        ";
-
-        await _db.SaveChanges(sql, mimbox);
     }
 
     public async Task<IEnumerable<Company>> GetMimboxDataByCompanyId(IEnumerable<Guid> ids)
