d0fbde5577de03540fe76a54ed469f7cc51b7202
Hargaaya <hargaaya10@gmail.com>
2022-12-05 20:30:53 +0100

    add mapping profile userinvitedto -> inviteduser

---
+++ Mimbly.Api/AAD/Mappings/AADMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿namespace Mimbly.Api.AAD.Mappings;
+
+using AutoMapper;
+using Mimbly.Api.AAD.DTOs;
+
+public class AADMappingProfile : Profile
+{
+    public AADMappingProfile()
+    {
+        CreateMap<UserInviteDTO, InvitedUser>()
+            .ForMember(
+            dest => dest.DisplayName,
+            dest => dest.MapFrom(src => string.Format("{1}{2}", src.FirstName, src.LastName
+            )))
+            .ForMember(dest => dest.Contact.JobTitle, dest => dest.MapFrom(src => src.JobTitle))
+            .ForMember(dest => dest.Contact.MobilePhone, dest => dest.MapFrom(src => src.Phone))
+            .ForMember(dest => dest.Contact.StreetAddress, dest => dest.MapFrom(src => src.StreetAddress))
+            .ForMember(dest => dest.Contact.City, dest => dest.MapFrom(src => src.City))
+            .ForMember(dest => dest.Contact.Country, dest => dest.MapFrom(src => src.Country));
+    }
+}
