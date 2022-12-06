7ac955d68148d148a1c9a040549497fb22a54ea1
Hargaaya <hargaaya10@gmail.com>
2022-12-06 18:14:33 +0100

    remove logger

--- Mimbly.Api/AAD/Helpers/GraphHelper.cs
+++ Mimbly.Api/AAD/Helpers/GraphHelper.cs
@@ -6,12 +6,10 @@ using Mimbly.Api.AAD.DTOs;
 public class GraphHelper : IGraphHelper
 {
     private readonly IGraphService _graphService;
-    private readonly ILogger<GraphHelper> _logger;
 
-    public GraphHelper(IGraphService graphService, ILogger<GraphHelper> logger)
+    public GraphHelper(IGraphService graphService)
     {
         _graphService = graphService;
-        _logger = logger;
     }
 
     public Invitation GetInvitation(InvitedUser user, string redirectUrl)
@@ -46,66 +44,33 @@ public class GraphHelper : IGraphHelper
     {
         var client = _graphService.GetClient();
 
-        try
-        {
             var resp = await client.Invitations.Request().AddAsync(invite);
             var userId = resp.InvitedUser.Id;
 
             return userId;
         }
-        catch (Exception ex)
-        {
-            // TODO: create a LoggerMessage Extension
-            _logger.LogInformation("Something went wrong inviting a user: ", ex);
-            return null;
-        }
-    }
 
     public async void UpdateUserInfo(User userInfo, string userId)
     {
         var client = _graphService.GetClient();
 
-        try
-        {
             await client.Users[userId].Request().UpdateAsync(userInfo);
         }
-        catch (Exception ex)
-        {
-            // TODO: create a LoggerMessage Extension
-            _logger.LogInformation("Something went wrong inviting a user: ", ex);
-        }
-    }
 
     public async void AddMemberToGroup(string groupId, string userId)
     {
         var client = _graphService.GetClient();
 
-        try
-        {
             var dirObj = new DirectoryObject { Id = userId };
             await client.Groups[groupId].Members.References.Request().AddAsync(dirObj);
         }
-        catch (Exception ex)
-        {
-            // TODO: create a LoggerMessage Extension
-            _logger.LogInformation("Something went wrong adding a member to a group: ", ex);
-        }
-    }
 
     public async void AddOwnerToGroup(string groupId, string userId)
     {
         var client = _graphService.GetClient();
 
-        try
-        {
             var dirObj = new DirectoryObject { Id = userId };
             await client.Groups[groupId].Owners.References.Request().AddAsync(new DirectoryObject { Id = userId });
         }
-        catch (Exception ex)
-        {
-            // TODO: create a LoggerMessage Extension
-            _logger.LogInformation("Something went wrong adding a member to a group: ", ex);
-        }
-    }
 
 }
