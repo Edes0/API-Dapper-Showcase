aac523c86c4f5f7962755260a6c779f1000e36fb
Hargaaya Idris <hargaaya10@gmail.com>
2022-12-07 15:31:06 +0100

    update models

--- Mimbly.Api/AAD/DTOs/CompanyModel.cs
+++ Mimbly.Api/AAD/DTOs/CompanyModel.cs
@@ -1,24 +1,8 @@
 ﻿namespace Mimbly.Api.AAD.DTOs;
 
-using FluentValidation;
-using Mimbly.Application.Contracts.Dtos.Company;
-using Mimbly.CoreServices.Validation;
-
-public class CompanyModel : CreateCompanyRequestDto
+public class CompanyModel
 {
+    public string? Name { get; set; }
     public string? Description { get; set; }
-
-    public new async Task Validate()
-    {
-        await ValidatableEntity.ValidateEntityByFluentRules(this, new CreateCompanyValidator());
-    }
-}
-
-internal class CreateCompanyValidator : AbstractValidator<CompanyModel>
-{
-    public CreateCompanyValidator()
-    {
-        RuleFor(company => company.Name).NotNull();
-        RuleFor(company => company.Description).NotNull();
-    }
+    public Guid ParentId { get; set; }
 }
--- Mimbly.Api/AAD/DTOs/CreateCompanyDTO.cs
+++ Mimbly.Api/AAD/DTOs/CreateCompanyDTO.cs
@@ -1,7 +1,26 @@
 ﻿namespace Mimbly.Api.AAD.DTOs;
 
+using Mimbly.CoreServices.Validation;
+using FluentValidation;
+
 public class CreateCompanyDTO
 {
-    public UserInviteDTO user { get; set; }
-    public CompanyModel company { get; set; }
+    public string? CompanyName { get; set; }
+    public string? Description { get; set; }
+    public Guid ParentId { get; set; }
+
+    public async Task Validate()
+    {
+        await ValidatableEntity.ValidateEntityByFluentRules(this, new CreateCompanyValidator());
+    }
+}
+
+internal class CreateCompanyValidator : AbstractValidator<CreateCompanyDTO>
+{
+    public CreateCompanyValidator()
+    {
+        RuleFor(company => company.CompanyName).NotNull().NotEmpty();
+        RuleFor(company => company.Description).NotNull().NotEmpty();
+        RuleFor(company => company.ParentId).NotNull().NotEmpty();
+    }
 }
