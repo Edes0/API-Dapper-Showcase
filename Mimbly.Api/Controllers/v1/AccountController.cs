339d360a71c1cfe7e5583e27a015f0a985a3c840
Hargaaya <hargaaya10@gmail.com>
2022-12-14 13:31:18 +0100

    uncomment authorize attribute

--- Mimbly.Api/Controllers/v1/AccountController.cs
+++ Mimbly.Api/Controllers/v1/AccountController.cs
@@ -11,7 +11,7 @@ using Mimbly.Application.Contracts.Dtos.Company;
 using MediatR;
 
 [ApiController]
-/*[Authorize]*/
+[Authorize]
 [Route("api/v{version:apiVersion}/[controller]")]
 [ApiVersion("1.0")]
 public class AccountController : ControllerBase
