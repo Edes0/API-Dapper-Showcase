ae00644403195c275eeb89abe0bc2ec161f978c0
Hargaaya Idris <hargaaya10@gmail.com>
2022-12-20 10:57:27 +0100

    add interfaces

---
+++ Mimbly.Business/Interfaces/AD/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿namespace Mimbly.Api.AAD;
+
+using Domain.Entities.AD;
+using Mimbly.Domain.Entities;
+
+public interface IAccountService
+{
+    Task<bool> InviteUser(AdUser user);
+    Task<bool> InviteTechnician(AdUser technician);
+    Task<bool> InviteAdmin(AdUser admin);
+    Task<string?> CreateCompany(AdCompany company);
+    Task<bool> AddUserToCompany(AdUser user, Guid companyId);
+}
---
+++ Mimbly.Business/Interfaces/AD/IGraphHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace Mimbly.Business.Interfaces.AD;
+
+using System.Threading.Tasks;
+using Domain.Entities.AD;
+using Microsoft.Graph;
+
+public interface IGraphHelper
+{
+    User GetUserInfo(AdUser user);
+    Invitation GetInvitation(AdUser user, string redirectUrl);
+    void AddMemberToGroup(string groupId, string userId);
+    void AddOwnerToGroup(string groupId, string userId);
+    Task<string?> InviteAndGetUserId(Invitation invite);
+    void UpdateUserInfo(User userInfo, string userId);
+}
