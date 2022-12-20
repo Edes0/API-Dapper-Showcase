d93703c77bbcea9c9889d8e358a24faf658bf123
Hargaaya Idris <hargaaya10@gmail.com>
2022-12-20 10:41:54 +0100

    add models

---
+++ Mimbly.Domain/Entities/AD/AdCompany.cs
@@ -0,0 +1,9 @@
+namespace Mimbly.Domain.Entities.AD;
+
+public class AdCompany
+{
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public Guid ParentId { get; set; }
+
+}
