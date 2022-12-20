b9664de0ec9fb91898a2a7919b773ec392f3ca74
Hargaaya Idris <hargaaya10@gmail.com>
2022-12-20 11:55:05 +0100

    register services from their new namespace

--- Mimbly.Api/Extensions/ServiceExtensions.cs
+++ Mimbly.Api/Extensions/ServiceExtensions.cs
@@ -1,5 +1,7 @@
 ﻿namespace Mimbly.Api.Extensions;
 
+using Business.Helpers.AD;
+using Business.Interfaces.AD;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -16,11 +18,6 @@ using Mimbly.CoreServices.PuppeteerServices;
 using Mimbly.Infrastructure.Identity.Context;
 using Mimbly.Persistence.Repositories;
 using PuppeteerSharp;
-using Microsoft.AspNetCore.Authorization;
-using Mimbly.CoreServices.Authorization;
-using Mimbly.Api.AAD;
-using Mimbly.Api.AAD.Helpers;
-using Mimbly.Api.AAD.Mappings;
 
 public static class PuppeteerExtensions
 {
@@ -70,7 +67,6 @@ public static class ServiceExtensions
     public static void ConfigureNugetPackages(this IServiceCollection services)
     {
         services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
-        services.AddAutoMapper(typeof(AADMappingProfile).Assembly);
         services.AddMediatR(typeof(ApplicationMediatREntrypoint).Assembly);
     }
 
