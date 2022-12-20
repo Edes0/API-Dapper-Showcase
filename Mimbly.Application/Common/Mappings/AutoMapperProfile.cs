b53909c84eb052de3dff4fbb8db19acb613b805d
Hargaaya Idris <hargaaya10@gmail.com>
2022-12-20 10:42:18 +0100

    add mapping

--- Mimbly.Application/Common/Mappings/AutoMapperProfile.cs
+++ Mimbly.Application/Common/Mappings/AutoMapperProfile.cs
@@ -1,6 +1,8 @@
 namespace Mimbly.Application.Common.Mappings;
 
 using AutoMapper;
+using Contracts.Dtos.AD;
+using Domain.Entities.AD;
 using Mimbly.Application.Contracts.Dtos.Company;
 using Mimbly.Application.Contracts.Dtos.CompanyContact;
 using Mimbly.Application.Contracts.Dtos.EventLog;
@@ -14,6 +16,15 @@ public class AutoMapperProfile : Profile
 {
     public AutoMapperProfile()
     {
+        // AD
+        CreateMap<InviteUserDto, AdUser>()
+            .ForMember(
+                dest => dest.DisplayName,
+                inp => inp.MapFrom(src => $"{src.FirstName} {src.LastName}"));
+
+        CreateMap<AddCompanyDto, AdCompany>()
+            .ForMember(dest => dest.Name, inp => inp.MapFrom(src => src.CompanyName));
+
         // Mimbox
         CreateMap<Mimbox, MimboxDto>();
 
