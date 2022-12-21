5027f5bccb598283ce489ea19f52435139661747
Hargaaya <hargaaya10@gmail.com>
2022-12-21 17:42:56 +0100

    add method to remove companies from ad

---
+++ Mimbly.Application/Commands/AD/RemoveCompanyFromAd/RemoveCompanyFromAdCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Mimbly.Application.Commands.AD.RemoveCompanyFromAd;
+
+using Business.Interfaces.AD;
+using MediatR;
+
+public class RemoveCompanyFromAdCommandHandler : IRequestHandler<RemoveCompanyFromAdCommand>
+{
+    private readonly IAccountService _ac;
+
+    public RemoveCompanyFromAdCommandHandler(IAccountService ac) => _ac = ac;
+
+    public async Task<Unit> Handle(RemoveCompanyFromAdCommand request, CancellationToken cancellationToken)
+    {
+        await _ac.RemoveCompany(request.Id);
+
+        return Unit.Value;
+    }
+}
