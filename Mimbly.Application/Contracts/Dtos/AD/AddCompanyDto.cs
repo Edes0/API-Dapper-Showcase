7c3f892739954be675c29971007065e0333b3bed
Hargaaya Idris <hargaaya10@gmail.com>
2022-12-22 10:59:08 +0100

    add annotations for validation

--- Mimbly.Application/Contracts/Dtos/AD/AddCompanyDto.cs
+++ Mimbly.Application/Contracts/Dtos/AD/AddCompanyDto.cs
@@ -1,15 +1,18 @@
 namespace Mimbly.Application.Contracts.Dtos.AD;
 
+using System.ComponentModel.DataAnnotations;
 using CoreServices.Validation;
 using FluentValidation;
 
 public class AddCompanyDto
 {
-
+    [Required]
     public string Name { get; set; } = string.Empty;
 
+    [Required]
     public string Description { get; set; } = string.Empty;
 
+    [Required]
     public Guid ParentId { get; set; }
 
     public async Task Validate()
--- Mimbly.Application/Contracts/Dtos/Mimbox/CreateMimboxRequestDto.cs
+++ Mimbly.Application/Contracts/Dtos/Mimbox/CreateMimboxRequestDto.cs
@@ -1,5 +1,6 @@
 namespace Mimbly.Application.Contracts.Dtos.Mimbox;
 
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using CoreServices.Validation;
 using Mimbly.Application.Common.Validators.Mimbox;
@@ -14,14 +15,18 @@ public record CreateMimboxRequestDto
 
     public float EconomySaved { get; init; }
 
+    [Required]
     public string? Nickname { get; set; }
 
+    [Required]
     public Guid StatusId { get; init; }
 
+    [Required]
     public Guid ModelId { get; init; }
 
     public Guid? LocationId { get; init; }
 
+    [Required]
     public Guid? CompanyId { get; set; }
 
     public async Task Validate()
