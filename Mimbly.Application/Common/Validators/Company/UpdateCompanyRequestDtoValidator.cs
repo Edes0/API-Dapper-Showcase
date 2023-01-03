0b0ebb25f5d40ee2321c708b25fa8a2c51fcfbbc
Andreas Sjögren <sjogrenandreas@live.se>
2023-01-03 12:34:56 +0100

    Add more validation rules

--- Mimbly.Application/Common/Validators/Company/UpdateCompanyRequestDtoValidator.cs
+++ Mimbly.Application/Common/Validators/Company/UpdateCompanyRequestDtoValidator.cs
@@ -1,6 +1,7 @@
 ﻿namespace Mimbly.Application.Common.Validators.Company;
 
 using FluentValidation;
+using Mimbly.Application.Common.Validators.CustomValidatorRules;
 using Mimbly.Application.Contracts.Dtos.Company;
 
 public class UpdateCompanyRequestDtoValidator : AbstractValidator<UpdateCompanyRequestDto>
@@ -8,6 +9,8 @@ public class UpdateCompanyRequestDtoValidator : AbstractValidator<UpdateCompanyR
     public UpdateCompanyRequestDtoValidator()
     {
         RuleFor(x => x.Name)
-           .NotNull().WithMessage("Name is required");
+            .NotEmpty().WithMessage("Name is required")
+            .Length(3, 55).WithMessage("Name length is invalid")
+            .Must(BeValidLetters.Validate).WithMessage("Name contains invalid characters");
     }
 }
--- Mimbly.Application/Common/Validators/CompanyContact/CreateCompanyContactRequestDtoValidator.cs
+++ Mimbly.Application/Common/Validators/CompanyContact/CreateCompanyContactRequestDtoValidator.cs
@@ -1,6 +1,7 @@
 ﻿namespace Mimbly.Application.Common.Validators.CompanyContact;
 
 using FluentValidation;
+using Mimbly.Application.Common.Validators.CustomValidatorRules;
 using Mimbly.Application.Contracts.Dtos.CompanyContact;
 
 public class CreateCompanyContactRequestDtoValidator : AbstractValidator<CreateCompanyContactRequestDto>
@@ -8,22 +9,30 @@ public class CreateCompanyContactRequestDtoValidator : AbstractValidator<CreateC
     public CreateCompanyContactRequestDtoValidator()
     {
         RuleFor(x => x.Title)
-                .NotNull().WithMessage("Title is required");
+            .NotEmpty().WithMessage("Title is required")
+            .Length(3, 55).WithMessage("Title length is invalid")
+            .Must(BeValidLetters.Validate).WithMessage("Title contains invalid characters");
 
         RuleFor(x => x.FirstName)
-               .NotNull().WithMessage("First name is required");
+            .NotEmpty().WithMessage("First name is required")
+            .Length(3, 55).WithMessage("First name length is invalid")
+            .Must(BeValidLetters.Validate).WithMessage("First name contains invalid characters");
 
         RuleFor(x => x.LastName)
-               .NotNull().WithMessage("Last name is required");
+            .NotEmpty().WithMessage("Last name is required")
+            .Length(3, 55).WithMessage("Last name length is invalid")
+            .Must(BeValidLetters.Validate).WithMessage("Last name contains invalid characters");
 
         RuleFor(x => x.Email)
-               .NotNull().WithMessage("Email is required")
-               .EmailAddress().WithMessage("Enter in the format: name@example.com");
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Enter in the format: name@example.com");
 
         RuleFor(x => x.PhoneNumber)
-               .NotNull().WithMessage("Phone number is required");
+            .NotEmpty().WithMessage("Phone number is required")
+            .Length(3, 55).WithMessage("Phone number length is invalid")
+            .Must(BeValidDigits.Validate).WithMessage("Phone number contains invalid characters");
 
         RuleFor(x => x.CompanyId)
-                .NotNull().WithMessage("Company is required");
+            .NotEmpty().WithMessage("Company is required");
     }
 }
