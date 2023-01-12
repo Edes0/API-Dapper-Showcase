d2b2b0ba1f1933acd3c8043e72935ecbfa6ecd0c
Hargaaya Idris <hargaaya10@gmail.com>
2023-01-12 14:25:01 +0100

    move caching logic out of controller

--- Mimbly.Api/Controllers/v1/AccountController.cs
+++ Mimbly.Api/Controllers/v1/AccountController.cs
@@ -2,12 +2,10 @@
 
 using Application.Commands.AD.InviteUserToAd;
 using Application.Contracts.Dtos.AD;
-using CoreServices.Authorization;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MediatR;
 using Mimbly.Application.Queries.AD.GetRoles;
-using Microsoft.Extensions.Caching.Memory;
 
 [ApiController]
 [Authorize]
@@ -16,12 +14,10 @@ using Microsoft.Extensions.Caching.Memory;
 public class AccountController : ControllerBase
 {
     private readonly IMediator _mediator;
-    private readonly IMemoryCache _memoryCache;
 
-    public AccountController(IMediator mediator, IMemoryCache memoryCache)
+    public AccountController(IMediator mediator)
     {
         _mediator = mediator;
-        _memoryCache = memoryCache;
     }
 
     [HttpPost]
@@ -38,12 +34,8 @@ public class AccountController : ControllerBase
     [Route("GetRoles")]
     public async Task<ActionResult> GetRoles()
     {
-        var rolesInCache = _memoryCache.Get<IEnumerable<RoleDto>>("Roles");
-        if (rolesInCache != null) return Ok(rolesInCache);
-
-        var memoryCacheOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromHours(12));
         var roles = await _mediator.Send(new GetRolesQuery());
-        _memoryCache.Set<IEnumerable<RoleDto>>("Roles", roles.Roles, memoryCacheOptions);
+
         return Ok(roles.Roles);
     }
 }
