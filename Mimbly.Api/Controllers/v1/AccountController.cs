2a299a12abbfbfbfc6f1eca8ae612479bbba7d4a
Hargaaya Idris <hargaaya10@gmail.com>
2023-01-12 14:13:59 +0100

    add endpoint to get roles

--- Mimbly.Api/Controllers/v1/AccountController.cs
+++ Mimbly.Api/Controllers/v1/AccountController.cs
@@ -1,14 +1,13 @@
 ﻿namespace Mimbly.Api.Controllers.v1;
 
-using Application.Commands.AD.AddCompanyToAd;
 using Application.Commands.AD.InviteUserToAd;
 using Application.Contracts.Dtos.AD;
 using CoreServices.Authorization;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Mimbly.Application.Commands.Company.CreateCompany;
-using Mimbly.Application.Contracts.Dtos.Company;
 using MediatR;
+using Mimbly.Application.Queries.AD.GetRoles;
+using Microsoft.Extensions.Caching.Memory;
 
 [ApiController]
 [Authorize]
@@ -17,10 +16,12 @@ using MediatR;
 public class AccountController : ControllerBase
 {
     private readonly IMediator _mediator;
+    private readonly IMemoryCache _memoryCache;
 
-    public AccountController(IMediator mediator)
+    public AccountController(IMediator mediator, IMemoryCache memoryCache)
     {
         _mediator = mediator;
+        _memoryCache = memoryCache;
     }
 
     [HttpPost]
@@ -31,5 +32,18 @@ public class AccountController : ControllerBase
         var status = await _mediator.Send(new InviteUserToAdCommand { InviteUserRequestToAdRequest = inviteUserRequestDto });
 
         return status ? Ok() : BadRequest();
+    }
+
+    [HttpGet]
+    [Route("GetRoles")]
+    public async Task<ActionResult> GetRoles()
+    {
+        var rolesInCache = _memoryCache.Get<IEnumerable<RoleDto>>("Roles");
+        if (rolesInCache != null) return Ok(rolesInCache);
+
+        var memoryCacheOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromHours(12));
+        var roles = await _mediator.Send(new GetRolesQuery());
+        _memoryCache.Set<IEnumerable<RoleDto>>("Roles", roles.Roles, memoryCacheOptions);
+        return Ok(roles.Roles);
     }
 }
