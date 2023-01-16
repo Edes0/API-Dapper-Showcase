049cbc0775764df264234938366c9868ef027df3
Andreas Sjögren <sjogrenandreas@live.se>
2023-01-16 15:02:03 +0100

    Change authorization

--- Mimbly.Api/Controllers/v1/MimboxLogController.cs
+++ Mimbly.Api/Controllers/v1/MimboxLogController.cs
@@ -4,6 +4,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Mimbly.Api.Attributes;
 using Mimbly.Application.Commands.MimboxLog.CreateMimboxLog;
 using Mimbly.Application.Commands.MimboxLog.DeleteMimboxLog;
 using Mimbly.Application.Commands.MimboxLog.UpdateMimboxLog;
@@ -12,27 +13,29 @@ using Mimbly.Application.Queries.MimboxLog.GetById;
 using Mimbly.Application.Queries.MimboxLog.GetByMimboxId;
 
 [ApiController]
-[Authorize]
 [Route("api/v{version:apiVersion}/[controller]")]
 [ApiVersion("1.0")]
-public class MimboxLogController : BaseController 
+public class MimboxLogController : BaseController
 {
     public MimboxLogController(IMediator mediator) : base(mediator)
     {
     }
 
+    [ApiKey]
     [HttpGet("ByMimboxId/{mimboxId:guid}", Name = "MimboxLogByMimboxId")]
     public async Task<ActionResult<MimboxLogsByMimboxIdVm>> FilterMimboxLogsByMimboxId([BindRequired] Guid mimboxId)
     {
         return Ok(await _mediator.Send(new GetByMimboxIdMimboxLogQuery { Id = mimboxId }));
     }
 
+    [Authorize]
     [HttpGet("{id:guid}", Name = "MimboxLogById")]
     public async Task<ActionResult<MimboxLogByIdVm>> FilterMimboxLogsById([BindRequired] Guid id)
     {
         return Ok(await _mediator.Send(new GetByIdMimboxLogQuery { Id = id }));
     }
 
+    [Authorize]
     [HttpPost]
     public async Task<ActionResult> CreateMimboxLog([FromBody] CreateMimboxLogRequestDto createMimboxLogRequestDto)
     {
@@ -41,6 +44,7 @@ public class MimboxLogController : BaseController
         return CreatedAtRoute("MimboxLogById", new { createdMimboxLog.Id }, createdMimboxLog);
     }
 
+    [Authorize]
     [HttpDelete("{id:guid}")]
     public async Task<ActionResult> DeleteMimboxLog([BindRequired] Guid id)
     {
@@ -49,6 +53,7 @@ public class MimboxLogController : BaseController
         return NoContent();
     }
 
+    [Authorize]
     [HttpPut("{id:guid}")]
     public async Task<ActionResult> UpdateMimboxLog(Guid id, [FromBody] UpdateMimboxLogRequestDto updateMimboxLogRequestDto)
     {
