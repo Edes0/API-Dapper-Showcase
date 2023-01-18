b5dde7c4d374390a025c031a11b090deb960f76b
Hargaaya <hargaaya10@gmail.com>
2023-01-18 15:37:03 +0100

    add documentation comments

--- Mimbly.Api/Extensions/ServiceExtensions.cs
+++ Mimbly.Api/Extensions/ServiceExtensions.cs
@@ -21,11 +21,17 @@ using PuppeteerSharp;
 
 public static class PuppeteerExtensions
 {
+    /// <summary>
+    /// Downloads and installs a chromium browser locally, to be
+    /// used headless for pdfs.
+    /// </summary>
+    /// <param name="service"></param>
+    /// <param name="hostingEnvironment"></param>
     public static async Task PreparePuppeteerAsync(this IServiceCollection service,
         IWebHostEnvironment hostingEnvironment)
     {
         // Downloads & Installs a chromium browser.
-        var downloadPath = Path.Join(AppDomain.CurrentDomain.BaseDirectory, "./puppeteer");
+        var downloadPath = Path.Join(System.IO.Directory.GetCurrentDirectory(), "./puppeteer");
         var browserOptions = new BrowserFetcherOptions { Path = downloadPath };
         var browserFetcher = new BrowserFetcher(browserOptions);
         ExecutablePath = browserFetcher.GetExecutablePath(BrowserFetcher.DefaultChromiumRevision);
@@ -37,9 +43,19 @@ public static class PuppeteerExtensions
 
 public static class ServiceExtensions
 {
+    /// <summary>
+    /// Configures the data access layer that
+    /// is used by data persistence services.
+    /// </summary>
+    /// <param name="services"></param>
     public static void ConfigureDataAccessManager(this IServiceCollection services) =>
           services.AddScoped<ISqlDataAccess, SqlDataAccess>();
 
+    /// <summary>
+    /// Configures services for data persistence for
+    /// multiple different entities.
+    /// </summary>
+    /// <param name="services"></param>
     public static void ConfigureRepositories(this IServiceCollection services)
     {
         services.AddScoped<IMimboxRepository, MimboxRepository>();
@@ -55,6 +71,12 @@ public static class ServiceExtensions
         services.AddScoped<IEventLogRepository, EventLogRepository>();
     }
 
+    /// <summary>
+    /// Configures cors according to the environment variable "corsUrl".
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="allowedOrigins"></param>
+    /// <param name="config"></param>
     public static void ConfigureCors(this IServiceCollection services, string allowedOrigins, IConfiguration config) =>
     services.AddCors(opts => opts.AddPolicy(allowedOrigins, policy =>
     {
@@ -64,17 +86,31 @@ public static class ServiceExtensions
         policy.AllowAnyHeader();
     }));
 
+    /// <summary>
+    /// Configures the SQL database for the application.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
     public static void ConfigureAppDbContext(this IServiceCollection services,
             IConfiguration configuration) =>
             services.AddDbContext<AppDbContext>(opts =>
             opts.UseSqlServer(configuration.GetConnectionString("DbConnectionString"), b => b.MigrationsAssembly("Mimbly.Api")));
 
+    /// <summary>
+    /// Configures AutoMapper and MediatR.
+    /// </summary>
+    /// <param name="services"></param>
     public static void ConfigureNugetPackages(this IServiceCollection services)
     {
         services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
         services.AddMediatR(typeof(ApplicationMediatREntrypoint).Assembly);
     }
 
+    /// <summary>
+    /// Configures authentication with Azure Ad.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configurationBuilder"></param>
     public static void ConfigureAuthentication(this IServiceCollection services, IConfigurationRoot configurationBuilder)
     {
         var azureAd = configurationBuilder.GetSection("AzureAd");
@@ -84,6 +120,12 @@ public static class ServiceExtensions
         .AddMicrosoftIdentityWebApi(azureAd);
     }
 
+    /// <summary>
+    /// Configures puppeteer that is used to create
+    /// pdf documents using a headless chrome instance.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="environment"></param>
     public static void ConfigurePuppeteer(this IServiceCollection services, IWebHostEnvironment environment)
     {
         services.AddControllersWithViews();
@@ -96,6 +138,11 @@ public static class ServiceExtensions
         });
     }
 
+    /// <summary>
+    /// Configures versioning for the controllers
+    /// and also enables it in Swagger.
+    /// </summary>
+    /// <param name="services"></param>
     public static void ConfigureVersioning(this IServiceCollection services)
     {
         services.AddApiVersioning(opt =>
@@ -119,12 +166,21 @@ public static class ServiceExtensions
         services.ConfigureOptions<SwaggerExtension>();
     }
 
+    /// <summary>
+    /// Configures role based authorization attribute.
+    /// </summary>
+    /// <param name="services"></param>
     public static void ConfigureAuthAttribute(this IServiceCollection services)
     {
         services.AddSingleton<IAuthorizationPolicyProvider, GroupsPolicyProvider>();
         services.AddSingleton<IAuthorizationHandler, GroupsHandler>();
     }
 
+    /// <summary>
+    /// Configures services and helpers to handle
+    /// Azure Ad entities, such as users and groups.
+    /// </summary>
+    /// <param name="services"></param>
     public static void ConfigureAccountService(this IServiceCollection services)
     {
         services.AddSingleton<IAccountService, AccountService>();
@@ -132,6 +188,11 @@ public static class ServiceExtensions
         services.AddSingleton<IGraphHelper, GraphHelper>();
     }
 
+    /// <summary>
+    /// Configures custom validation responses according to
+    /// the requirements from the frontend.
+    /// </summary>
+    /// <param name="services"></param>
     public static void ConfigureCustomValidationResponse(this IServiceCollection services)
     {
         services.Configure<ApiBehaviorOptions>(options =>
